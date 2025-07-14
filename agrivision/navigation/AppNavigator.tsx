d4c6ff62e6efa316b88933f8c571c297d8ee45dc
PritAm7355 <pritamtk236@gmail.com>
2025-07-14 19:53:21 +0530

    update header with device status

--- agrivision/navigation/AppNavigator.tsx
+++ agrivision/navigation/AppNavigator.tsx
@@ -6,70 +6,123 @@ import PumpLogsScreen from '../screens/PumpLogsScreen';
 import UltrasonicLogsScreen from '../screens/UltrasonicLogsScreen';
 import SplashScreen from '../screens/SplashScreen';
 import HomeScreen from '../screens/HomeScreen';
+import AgriScreenData from '../screens/AgriDataScreen';
+import UploadPage from '../screens/UploadImgScreen';
+import ScanScreen from '../screens/Satellite/ScanScreen';
+import MapScreen from '../screens/Satellite/MapScreen';
+import { RootStackParamList } from './types';
+import DeviceScreen from '../screens/Device/DeviceScreen';
+import InsideDeviceScreen from '../screens/Device/InsideDevScreen';
+import DeviceStatus from '../components/DeviceStatus';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        {/* Splash has no header */}
+      <Stack.Navigator initialRouteName="Splash"
+        screenOptions={{
+          headerRight: () => <DeviceStatus />,
+        }}>
+
         <Stack.Screen
           name="Splash"
           component={SplashScreen}
           options={{ headerShown: false }}
         />
-
-        {/* Home screen with header */}
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{
-            title: 'Agri Vision', // Header title
-            headerStyle: {
-              backgroundColor: '#4CAF50', // Header background
-            },
-            headerTintColor: '#fff', // Text and back arrow color
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontSize: 20,
-            },
+            title: 'Agri Vision',
+            headerStyle: { backgroundColor: '#4CAF50' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
           }}
         />
-
         <Stack.Screen
           name="PumpLogs"
           component={PumpLogsScreen}
           options={{
             title: 'Pump Logs',
-            headerStyle: {
-              backgroundColor: '#4CAF50',
-            },
+            headerStyle: { backgroundColor: '#4CAF50' },
             headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontSize: 20,
-            },
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
           }}
         />
-
         <Stack.Screen
           name="UltrasonicLogs"
           component={UltrasonicLogsScreen}
           options={{
             title: 'Ultrasonic Logs',
-            headerStyle: {
-              backgroundColor: '#4CAF50',
-            },
+            headerStyle: { backgroundColor: '#4CAF50' },
             headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontSize: 20,
-            },
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
+          }}
+        />
+        <Stack.Screen
+          name="AgriScreenData"
+          component={AgriScreenData}
+          options={{
+            title: 'Agri Data',
+            headerStyle: { backgroundColor: '#4CAF50' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
+          }}
+        />
+        <Stack.Screen
+          name="UploadPage"
+          component={UploadPage}
+          options={{
+            title: 'Upload Plant Image',
+            headerStyle: { backgroundColor: '#4CAF50' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
+          }}
+        />
+        <Stack.Screen
+          name="ScanScreen"
+          component={ScanScreen}
+          options={{
+            title: 'Scan Field',
+            headerStyle: { backgroundColor: '#4CAF50' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
+          }}
+        />
+        
+        <Stack.Screen
+          name="MapScreen"
+          component={MapScreen}
+          options={{
+            title: 'Satellite Map',
+            headerStyle: { backgroundColor: '#4CAF50' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
           }}
         />
 
-        
+        <Stack.Screen
+          name="DeviceScreen"
+          component={DeviceScreen}
+          options={{
+            title: 'Device',
+            headerStyle: { backgroundColor: '#4CAF50' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
+          }}
+        />
+
+        <Stack.Screen
+          name="InsideDeviceScreen"
+          component={InsideDeviceScreen}
+          options={{
+            title: 'Device',
+            headerStyle: { backgroundColor: '#4CAF50' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 20 },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
